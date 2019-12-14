aa3972d54641e43987d1a97ffefa2f8053721036
Erwin <evancrasbeek@xs4all.nl>
2019-12-14 20:57:49 +0100

    Improved test script.

--- BuildScripts/Test.sh
+++ BuildScripts/Test.sh
@@ -1,1 +1,5 @@
+echo Starting tests
 'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -quit -runTests -batchmode -projectPath ../LaneRunner/ -testResults C:\temp\results.xml -testPlatform StandaloneWindows64
+
+echo Printing results
+cat \temp\results.xml
