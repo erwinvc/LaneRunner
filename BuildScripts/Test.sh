55244de44ccc848bec5777a0650aa036585eae5a
Erwin <evancrasbeek@xs4all.nl>
2019-12-14 22:05:09 +0100

    Imrpved test script.

--- BuildScripts/Test.sh
+++ BuildScripts/Test.sh
@@ -1,5 +1,5 @@
 echo Starting tests
-'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -quit -runTests -batchmode -projectPath ../LaneRunner/ -testResults C:\temp\results.xml -testPlatform StandaloneWindows64
+'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -runTests -projectPath '../LaneRunner/' -logfile '$WORKSPACE/Tests/log.xml' -testResults '$WORKSPACE/Tests/results.xml' -testPlatform playmode
 
 echo Printing results
-cat \temp\results.xml
+cat '$WORKSPACE/Tests/results.xml'
