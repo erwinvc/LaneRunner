4846180cc6de304f2b33dd06994505ea0d8424ef
Erwin <evancrasbeek@xs4all.nl>
2019-12-14 20:49:21 +0100

    Added tests; Added test shell script for Jenkins.

---
+++ BuildScripts/Test.sh
@@ -0,0 +1,1 @@
+'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -quit -runTests -batchmode -projectPath ../LaneRunner/ -testResults C:\temp\results.xml -testPlatform StandaloneWindows64
