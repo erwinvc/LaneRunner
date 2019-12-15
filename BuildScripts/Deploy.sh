47d8faec3e2a06478e29512a1fb4b7dfd88c8a5a
Erwin <evancrasbeek@xs4all.nl>
2019-12-15 15:15:52 +0100

    Improved deploy script.

--- BuildScripts/Deploy.sh
+++ BuildScripts/Deploy.sh
@@ -2,5 +2,5 @@ echo Cleaning Up Build Directory
 rm -rf ../LaneRunner/Build/
 
 echo Starting Build Process
-'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -quit -bachmode -projectPath ../LaneRunner/ -executeMethod BuildScript.PerformBuild
+'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -nographics -quit -bachmode -projectPath ../LaneRunner/ -executeMethod BuildScript.PerformBuild ${JOB_NAME} "${JENKINS_HOME}"/jobs/${JOB_NAME}/builds/"${BUILD_NUMBER}"/output
 echo Ended Build Process
--- BuildScripts/Test.sh
+++ BuildScripts/Test.sh
@@ -1,5 +1,2 @@
 echo Starting tests
-'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -runTests -projectPath '../LaneRunner/' -logfile '$WORKSPACE/Tests/log.xml' -testResults '$WORKSPACE/Tests/results.xml' -testPlatform playmode
-
-echo Printing results
-cat '$WORKSPACE/Tests/results.xml'
+'D:/Program Files/Unity installs/2019.2.5f1/Editor/Unity.exe' -runTests -batchmode -projectPath '../LaneRunner/' -logfile '$WORKSPACE/Tests/log.xml' -testResults '$WORKSPACE/Tests/results.xml' -testPlatform playmode
