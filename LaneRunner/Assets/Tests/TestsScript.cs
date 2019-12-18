7fad03825e50f524306220f5b0e05c648ac76df7
Erwin <evancrasbeek@xs4all.nl>
2019-12-18 13:23:47 +0100

    Moved build scripts to Editor folder.

--- LaneRunner/Assets/Tests/TestsScript.cs
+++ LaneRunner/Assets/Tests/TestsScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Tests {
